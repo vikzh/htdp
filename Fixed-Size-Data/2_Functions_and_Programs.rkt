acd90669980374cb609d127ceaa875418de0b88f
vikzh <vikt.anmail@gmail.com>
2021-01-04 06:06:40 +0300

    2.5 Programs added

--- Fixed-Size-Data/2_Functions_and_Programs.rkt
+++ Fixed-Size-Data/2_Functions_and_Programs.rkt
@@ -49,3 +49,57 @@
 (define (profit ticket-price)
   (- (revenue ticket-price)
      (cost ticket-price)))
+
+;Programs
+;31-31
+(require 2htdp/batch-io)
+
+(define (letter fst lst signature-name)
+  (string-append
+    (opening fst)
+    "\n\n"
+    (body fst lst)
+    "\n\n"
+    (closing signature-name)))
+ 
+(define (opening fst)
+  (string-append "Dear " fst ","))
+ 
+(define (body fst lst)
+  (string-append
+   "We have discovered that all people with the" "\n"
+   "last name " lst " have won our lottery. So, " "\n"
+   fst ", " "hurry and pick up your prize."))
+ 
+(define (closing signature-name)
+  (string-append
+   "Sincerely,"
+   "\n\n"
+   signature-name
+   "\n"))
+
+(define (main out)
+  (write-file out
+              (letter (read-file 'stdin)
+                      (read-file 'stdin)
+                      (read-file 'stdin))))
+
+
+;world state
+(require 2htdp/universe)
+
+(define BACKGROUND (empty-scene 100 100))
+(define DOT (circle 3 "solid" "red"))
+ 
+(define (main y)
+  (big-bang y
+    [on-tick sub1]
+    [stop-when zero?]
+    [to-draw place-dot-at]
+    [on-key stop]))
+ 
+(define (place-dot-at y)
+  (place-image DOT 50 y BACKGROUND))
+ 
+(define (stop y ke)
+  0)
