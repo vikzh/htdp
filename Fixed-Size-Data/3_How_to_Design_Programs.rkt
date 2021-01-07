05dda2394c75e7dbcf21eb2aebeaf6ebd6cc5dad
vikzh <vikt.anmail@gmail.com>
2021-01-07 04:22:48 +0300

    39 added

--- Fixed-Size-Data/3_How_to_Design_Programs.rkt
+++ Fixed-Size-Data/3_How_to_Design_Programs.rkt
@@ -34,3 +34,25 @@
 ; given: "Hello World", expect: "Hello Worl"
 (define (string-remove-last string)
   (substring string 0 (- (string-length string) 1)))
+
+
+;39
+(define WHEEL-RADIUS 10)
+(define WHEEL-DISTANCE (* WHEEL-RADIUS 2))  
+
+(define WHEEL                                                         
+  (circle WHEEL-RADIUS "solid" "black"))    
+(define SPACE                             
+  (rectangle WHEEL-DISTANCE (/ WHEEL-RADIUS 8) "solid" "white"))
+(define BOTH-WHEELS                       
+  (beside WHEEL SPACE WHEEL))
+
+(define CHASSIS
+  (above
+   (rectangle (* WHEEL-DISTANCE 2) WHEEL-RADIUS "solid" "red")
+   (rectangle (* WHEEL-DISTANCE 4) (* WHEEL-RADIUS 2) "solid" "red")))
+
+(overlay/align/offset "middle" "bottom"
+  BOTH-WHEELS
+   0 (* WHEEL-RADIUS -1)
+   CHASSIS)
