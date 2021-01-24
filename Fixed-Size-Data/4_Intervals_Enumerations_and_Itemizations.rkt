2e081084df1f70f30fd415a642629d84425238ce
vikzh <vikt.anmail@gmail.com>
2021-01-24 14:09:10 +0300

    51 added

--- Fixed-Size-Data/4_Intervals_Enumerations_and_Itemizations.rkt
+++ Fixed-Size-Data/4_Intervals_Enumerations_and_Itemizations.rkt
@@ -10,3 +10,22 @@
     [(string=? "green" s) "yellow"]
     [(string=? "yellow" s) "red"]))
 
+;51
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define (tock cw)
+  (traffic-light-next cw))
+
+; TrafficLight -> Image
+(check-expect (render "green") (circle 30 "solid" "green"))
+(define (render current-light)
+  (circle 30 "solid" current-light))
+
+; TrafficLight -> TrafficLight
+(define (main init-color)
+  (big-bang init-color
+    [on-tick tock 3]
+    [to-draw render]))
+
+(main "red")
